51a9f5d82537969cc2f829ed159b0306689c8f63
Dx <dxcode01@gmail.com>
2024-01-09 00:18:47 -0600

    1

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,27 @@
-const width = document.querySelector('#i-button');
+function fizzBuzz (max){
+    let fizz = 0 ;
+    let fuzz = 0 ;
+    for (let i = 1 ; i <= max ; i ++){ 
+        let resFizz = i / 3 ;
+        let resFuzz = i / 5 ;
+        if (Number.isInteger(resFizz) ||  Number.isInteger(resFuzz)){
+            if (Number.isInteger(resFizz) && Number.isInteger(resFuzz)){
+                fizz ++ ; fuzz ++ ;
+            }
+            else if (Number.isInteger(resFizz)) {
+                fizz ++ ;
+            }
+            else if (Number.isInteger(resFuzz)) {
+                fuzz ++ ;
+            }
+        }
+    }
+    console.log (`fizz = ${fizz} , fuzz = ${fuzz}`);
+} 
 
-width.addEventListener('click' , e => {
-    
-});
+function test () {
+    let array = [ 1 , 15 , 70 ]
+    for (x of array){
+        fizzBuzz(x);
+    }
+}
